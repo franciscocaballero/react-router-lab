db54dd6ad569ecbe381640549b299b72db00fc66
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-03-29 14:39:07 -0400

    initail commit

---
+++ Components/Search.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+ import './Search.css'
+
+ class Search extends Component{
+    render(){
+      return(
+         <div>
+
+        </div>
+       )
+    }
+ }
+
+ export default Search
+View
+4
